2304bf05ffc84bae8b4f794d940cfee9fba0469d
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-01-08 19:29:06 +0530

    Add solution14

---
+++ tree/two-sum-iv-input-is-a-bst/mySolution.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void inorder(TreeNode* root , vector<int> &v){
+        if(!root) return;
+        inorder(root->left , v);
+        v.push_back(root->val);
+        inorder(root->right , v);
+    }
+    
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> v;
+        inorder(root , v);
+        int l  = 0 , h = v.size() - 1;
+        while(l<h){
+            if(v[l] + v[h] == k)
+                return true;
+            else if(v[l] + v[h] < k){
+                l++;
+            }
+            else{
+                h--;
+            }
+        }
+        return false;
+    }
+};
+
+//T.C: O(N) , S.C: O(N)
