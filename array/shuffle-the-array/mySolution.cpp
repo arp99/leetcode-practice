efbb689065284265280c31e57b8121bccfdb045a
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-01-28 01:34:36 +0530

    Add solution35

---
+++ array/shuffle-the-array/mySolution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res;
+        int i = 0 , j = n;
+        for(;i<n;i++ , j++){
+            res.push_back(nums[i]);
+            res.push_back(nums[j]);
+        }
+        return res;
+    }
+};
