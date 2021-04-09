15ce5cf0b78609635bbb9c6e57d21dcaca5b8b2c
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-04-09 22:27:28 +0530

    Add solution54

---
+++ array/find-all-duplicates-in-an-array/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[abs(nums[i]) - 1] < 0){
+                res.push_back(abs(nums[i]));
+            }
+            nums[abs(nums[i]) - 1] = -1*nums[abs(nums[i]) - 1];
+        }
+        return res;
+    }
+};
